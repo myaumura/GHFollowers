ccd0c5e23360b780f0016e09cd2d91f5837049fb
Kirill Gusev <k3dosh@gmail.com>
2023-09-30 15:02:50 +0300

    added Network Manager

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -9,7 +9,7 @@ import UIKit
 
 
 class FollowerListVC: UIViewController {
-
+    
     var username: String!
     
     override func viewDidLoad() {
@@ -17,10 +17,22 @@ class FollowerListVC: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
         
+        NetworkManager.shared.getFollowers(for: username, page: 1) { (result) in
+            
+            switch result {
+            case .success(let followers):
+                print(followers)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Bad Stuff Happens",
+                                                message: error.rawValue ,
+                                                buttonTitle: "Ok")
+            }
+        }
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(false, animated: true)
-    }
+        override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            navigationController?.setNavigationBarHidden(false, animated: true)
+        }
+    
 }
